6bdf340677dea62d6c64aa3ca489064d88da0a97
Emanuel Victor Fontoura Cantalejo <emanuelfontoura03@gmail.com>
2022-11-11 20:03:48 -0300

    transferência de arquivos

---
+++ Exercícios/aula026/mq005/script.js
@@ -0,0 +1,17 @@
+var menu = window.document.getElementById('imenu')
+
+function abrirMenu(){
+    if (menu.style.display == "block"){
+        menu.style.display = "none"
+    }else{
+        menu.style.display = "block"
+    }
+}
+
+function mudouTamanho(){
+    if (window.innerWidth >= 768){
+        menu.style.display = "block"
+    }else{
+        menu.style.display = "none"
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# estudo-html-css-cursoemvideo
- Repositório para estudo de HTML5 e CSS3.
+# html-css
+ Curso de HTML5 e CSS3
+
+
+Estou aprendendo a criar sites com HTML5 e CSS3
+
+<a href="https://emanuelvfc.github.io/html-css/Exerc%C3%ADcios/desaf010/index.html">Execute o Site Android aqui</a>
